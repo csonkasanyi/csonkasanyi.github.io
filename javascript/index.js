2de78b4bd7e00fab450d855fd3789b91eef46e77
csonkasanyi <81265993+csonkasanyi@users.noreply.github.com>
2021-04-14 12:15:16 +0200

    href correction

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+
+
+TweenMax.fromTo(".title",1.5,{ y: -500 },{ y:400,ease: Bounce.easeOut });
+TweenMax.to(".bemutatkozo",1,{scale:1, backgroundColor:"rgba(255,255,255,0.5)"});
---
+++ javascript/index_javascript.js
@@ -0,0 +1,12 @@
+/*var kedvencSzam1 = prompt('Add meg a kedvenc számod!');
+var kedvencSzam2 = prompt('Add meg a másik kedvenc számod!');
+
+if (kedvencSzam1 == kedvencSzam2) {
+	alert('Ugyanazt a számot adtad meg 2szer!');
+} else if (kedvencSzam1 > kedvencSzam2) {
+	alert('Az elsőként megadott szám nagyobb, mint a második!');
+}
+else {
+	alert('A 2. szám volt a nagyobb!');
+}*/
+document.div.onmouseover.backgroundColor = "yellow";
